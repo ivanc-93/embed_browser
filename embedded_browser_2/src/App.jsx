c7233f3e55be6f0b7c51ebbb2a5c3b3973802440
ivujgaleno <ivujgaleno@gmail.com>
2023-10-16 16:44:18 -0600

    primer commit

---
+++ embedded_browser_2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div  >
+      <iframe style={{width:"1700px",height:"1500px"}} src="https://www.remotedesktop.google.com" title="W3Schools Free Online Web Tutorials"></iframe>
+    </div>
+  )
+}
+
+export default App
